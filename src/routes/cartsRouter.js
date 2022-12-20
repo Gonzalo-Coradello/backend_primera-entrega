90b65858e8807ee56db685d999511ff33b8171e7
Gonzalo Coradello <gonzalocoradello@gmail.com>
2022-12-19 21:32:49 -0300

    funcionalidad terminada

--- src/routes/cartsRouter.js
+++ src/routes/cartsRouter.js
@@ -1,20 +1,37 @@
 import { Router } from 'express'
+import CartManager from '../CartManager.js'
 
 const router = Router()
+const cartManager = new CartManager('./carts.json')
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // Crear nuevo carrito con la estructura:
     // id (autogenerado), products (array de objetos)
+
+    const cart = await cartManager.createCart()
+    res.json({cart})
 })
 
-router.get('/:cid', (req, res) => {
+router.get('/:cid', async (req, res) => {
     // Mostrar productos que pertenezcan a dicho carrito
+    const cid = req.params.cid
+
+    const products = await cartManager.getProducts(cid)
+    
+    res.json({products})
 })
 
 
-router.post('/:cid/product/:pid', (req, res) => {
+router.post('/:cid/products/:pid', async (req, res) => {
     // agregar el producto al arreglo “products” del carrito seleccionado, agregándose como un objeto bajo el siguiente formato:
     // product (sólo el id), quantity
+
+    const cid = req.params.cid
+    const pid = req.params.pid
+
+    await cartManager.addProduct(cid, pid)
+
+    res.send('Producto agregado')
 })
 
 
